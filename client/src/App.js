283fd340ddf8bf6e3c81fe047746c7edae9fa844
Khigoris <foreveralonefoxy1@gmail.com>
2021-04-07 10:16:16 +0200

    Show users email

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,20 @@ import Axios from 'axios'
 export default function App() {
   const [userpicture, setUserpicture] = useState('')
   const [username, setUsername] = useState('')
+  const [useremail, setUseremail] = useState('')
 
   const getUser = () => {
     Axios.get('https://randomuser.me/api/').then(response => {
       setUsername(response.data.results["0"].name)
       setUserpicture(response.data.results["0"].picture.large)
+      setUseremail(response.data.results["0"].email)
     })
   }
 
   return (
     <div>
-      <img src={userpicture} />
+      <p>{useremail}</p>
+      <img src={userpicture} alt="" />
       <p>{username.title} {username.first} {username.last}</p>
 
       <button onClick={getUser}>Get User</button>
