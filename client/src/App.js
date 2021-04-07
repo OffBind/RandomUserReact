baca873e30452002f336db996519282154e88739
Khigoris <foreveralonefoxy1@gmail.com>
2021-04-07 10:21:55 +0200

    Show users age

--- client/src/App.js
+++ client/src/App.js
@@ -5,20 +5,23 @@ export default function App() {
   const [userpicture, setUserpicture] = useState('')
   const [username, setUsername] = useState('')
   const [useremail, setUseremail] = useState('')
+  const [userage, setUserage] = useState('')
 
   const getUser = () => {
     Axios.get('https://randomuser.me/api/').then(response => {
       setUsername(response.data.results["0"].name)
       setUserpicture(response.data.results["0"].picture.large)
       setUseremail(response.data.results["0"].email)
+      setUserage(response.data.results["0"].dob.age)
     })
   }
 
   return (
     <div>
-      <p>{useremail}</p>
+      <p>Email: {useremail}</p>
       <img src={userpicture} alt="" />
-      <p>{username.title} {username.first} {username.last}</p>
+      <p>Name: {username.title} {username.first} {username.last}</p>
+      <p>Age: {userage}</p>
 
       <button onClick={getUser}>Get User</button>
     </div>
